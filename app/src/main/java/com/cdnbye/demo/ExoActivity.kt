ce483a002e978abb1b42dea30e2fa4e28f6446a4
snowinszu <86755838@qq.com>
2023-12-15 16:23:51 +0800

    update hls source

--- app/src/main/java/com/cdnbye/demo/ExoActivity.kt
+++ app/src/main/java/com/cdnbye/demo/ExoActivity.kt
@@ -16,7 +16,7 @@ import com.p2pengine.sdk.P2pEngine
 import java.lang.RuntimeException
 
 class ExoActivity : BaseActivity() {
-    private val hls1 = "https://video.cdnbye.com/0cf6732evodtransgzp1257070836/e0d4b12e5285890803440736872/v.f100220.m3u8"
+    private val hls1 = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
     private val hls2 = "https://stream.swarmcloud.net:2096/hls/sintel/playlist.m3u8"
     private val dash1 = "https://bitmovin-a.akamaihd.net/content/MI20192708/stream.mpd"
     private val dash2 = "https://dash.akamaized.net/akamai/test/caption_test/ElephantsDream/elephants_dream_480p_heaac5_1.mpd"
